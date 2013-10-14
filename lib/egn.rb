7c6cb18dce13951bedfe8d6d948d76e623e9f6a2
gmitrev <gvmitrev@gmail.com>
2013-10-14 20:25:40 +0300

    EGN validator added

--- lib/egn.rb
+++ lib/egn.rb
@@ -1,11 +1,11 @@
-require "egn/version"
+# require "egn/version"
 
 module Egn
   PARITY_WEIGHTS = [2,4,8,5,10,9,7,3,6]
 
   module Generator
     def self.egn
-      year = Random.rand(1800..2000)
+      year = Random.rand(1800..2099)
       mon = Random.rand(1..12)
       day = Random.rand(1..31)
       cent = year - (year % 100)
@@ -38,9 +38,35 @@ module Egn
       rest < 10 ? rest : 0
     end
 
+
   end
 
   module Validator
+    def self.egn(egn)
+      return false unless egn.length == 10
+
+      year, month, day = egn.scan(/.{1,2}/)
+      month = month.to_i
+      day = day.to_i
+
+      case month
+      when (1..12)
+        year = "19#{year}"
+      when (21..32)
+        month -= 20
+        year = "20#{year}"
+      when (41..52)
+        month -= 40
+        year = "18#{year}"
+      end
+      year = year.to_i
+
+      return false unless Date.valid_date? year, month, day
+
+      checksum = Generator.egn_checksum egn[0,9]
+
+      checksum == egn[9].to_i
+    end
 
   end
 
