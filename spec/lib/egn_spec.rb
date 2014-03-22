e21cbfb6dfeca8069fd4701f316cf812b0b6bfd4
gmitrev <gvmitrev@gmail.com>
2014-03-22 20:50:54 +0200

    Tests for generating an EGN

--- spec/lib/egn_spec.rb
+++ spec/lib/egn_spec.rb
@@ -20,12 +20,39 @@ describe "Egn" do
       end
 
     end
-  end
 
-  describe 'generating' do
+    context "invoked with an EGN " do
 
-    it 'creates a new'
+      it "returns a new Egn object if the provided EGN is valid" do
+        egn = Egn::Egn.new('6101047500')
+        
+        expect(egn).to be_valid
+      end
 
+      it "raises an ArgumentError if the provided EGN is not valid" do
+        expect{
+          Egn::Egn.new("I'm invalid")
+        }.to raise_error ArgumentError
+      end
+
+    end
+
+    context "inoked with an options hash" do
+
+      it "generates a new EGN considering the given options" do
+        egn = Egn::Egn.new(year: 1990, month: 12, day: 30, sex: :male)
+
+        expect(egn).to be_valid
+      end
+
+      it "generates a new EGN with the given year" do
+        egn = Egn::Egn.new(year: 1990)
+
+        expect(egn.year).to eq(1990)
+      end
+
+
+    end
   end
 
   describe 'validating'
