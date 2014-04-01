025b3dfbec2dce09b938e38d02c889186f548fd5
gmitrev <gvmitrev@gmail.com>
2014-04-01 15:04:37 +0300

    Move things around in Generator

--- lib/egn/generator.rb
+++ lib/egn/generator.rb
@@ -5,24 +5,15 @@ module Egn
     # The generated EGN will be completely random if no opitons are given.
     # options is a hash that may have the following keys: :year, :month and :date
     def self.generate(options={})
-      date = Util.time_rand
-
-      options = {
-        year:  date.year,
-        month: date.month,
-        day:   date.day
-      }.merge(options)
+      options = defaults.merge(options)
 
       validate!(options)
 
       century = options[:year] - (options[:year] % 100)
       sex = Random.rand(1..2)
 
-      if century == 1800
-        options[:month] += 20
-      elsif century == 2000
-        options[:month] += 40
-      end
+      options[:month] += 20 if century == 1800
+      options[:month] += 40 if century == 2000
 
       region = Random.rand(0..999)
 
@@ -33,7 +24,10 @@ module Egn
       end
 
       final_year = options[:year] - century
-      egn = final_year.to_s.rjust(2, '0') + options[:month].to_s.rjust(2, '0') + options[:day].to_s.rjust(2,'0') + region.to_s.rjust(3,'0')
+      egn = final_year.to_s.rjust(2, '0') +
+            options[:month].to_s.rjust(2, '0') +
+            options[:day].to_s.rjust(2,'0') +
+            region.to_s.rjust(3,'0')
 
       return egn + Util.egn_checksum(egn).to_s
     end
@@ -43,6 +37,15 @@ module Egn
       raise ArgumentError, "Year out of bounds" unless (1800..2099).include?(options[:year])
       raise ArgumentError, "Month out of bounds" unless (1..12).include?(options[:month])
       raise ArgumentError, "Day out of bounds" unless (1..31).include?(options[:day])
+    end
+
+    def self.defaults
+      date = Util.time_rand
+      {
+        year:  date.year,
+        month: date.month,
+        day:   date.day
+      }
     end
 
   end
