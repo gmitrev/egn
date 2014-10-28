c4964e3027ad2062b8228b011570c858fe2cbf97
gmitrev <gvmitrev@gmail.com>
2014-10-28 15:55:08 +0200

    Improved validator spec

--- lib/egn/version.rb
+++ lib/egn/version.rb
@@ -1,3 +1,3 @@
 module Egn
-  VERSION = "1.2.0"
+  VERSION = "1.2.1"
 end
--- spec/egn/validator_spec.rb
+++ spec/egn/validator_spec.rb
@@ -14,9 +14,8 @@ describe Egn::Validator do
       expect(Egn::Validator.validate("6101347500")).to be_false
     end
 
-    it "checks 500 000 of the generated numbers", :brute  do
-      egns = Array.new(500_000).map{ |i| Egn.generate }
-      egns.each do |egn|
+    it "checks 10 000 of the generated numbers" do
+      Array.new(10_000) { |i| Egn.generate }.each do |egn|
         result = Egn::Validator.validate(egn)
         puts egn unless result
         expect(result).to be_true
